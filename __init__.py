2052906a8271dea5a96ae1051f73ff2dcbf36117
Tapanmittal13 <40453897+Tapanmittal13@users.noreply.github.com>
2019-09-10 22:39:35 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 22:37:48 2019
+
+@author: Tapan_Mittal
+"""
+
+from .data import *
+from .inspect import *
+from .misc import *
+from .model import *
+from .optimization import *
